79824b5f2342a1a64f8f21d9bfa65bde794acc15
Micha Alt <mail@lean-stack.de>
2019-07-22 10:47:09 +0200

    ToggleAll checkbox implemented.

--- src/todos/TodoMain.tsx
+++ src/todos/TodoMain.tsx
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import TodoList from './TodoList';
-import { useAppState } from './store';
+import { useAppState, useDispatch, setAllStates } from './store';
 
 const TodoMain = () => {
 
   const todos = useAppState(state => state.todos);
+  const dispatch = useDispatch();
+
+  // No need to recalculate if visibilty filter changes
+  const allCompleted = useMemo(() => {
+    return todos.findIndex(t => !t.completed) === -1
+  }, [todos]);
+
+  const setAllCompletedStates = () => {
+    dispatch(setAllStates(!allCompleted));
+  };
 
   return (
     <section className="main">
-      <input id="toggle-all" className="toggle-all" type="checkbox" />
+      <input id="toggle-all" className="toggle-all" type="checkbox"
+        checked={allCompleted} onChange={setAllCompletedStates} />
       <label htmlFor="toggle-all">Mark all as complete</label>
       <TodoList todos={todos}></TodoList>
     </section>
